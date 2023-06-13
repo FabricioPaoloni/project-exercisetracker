0a105b3e55f4413f394ebebf0c092bc5c745dfa7
Fabricio Paoloni <paolonifabricio@gmail.com>
2023-06-13 14:33:44 +0200

    Completed the GET method for getting de LOGS of an user

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const listener = app.listen(process.env.PORT || 3000, () => {
 })
 
 //Connect to the database:
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, UseUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, UseUnifiedTopology: true });
 
 //test de connection with MongoDB and log to the console
 let connection = mongoose.connection;
@@ -37,17 +37,17 @@ const User = mongoose.model('User', userSchema);
 
 
 //POST a new user
-app.post('/api/users', async function(req, res){
+app.post('/api/users', async function (req, res) {
   let newUsername = req.body.username;
 
-  // if(newUsername === null | newUsername === ""){res.json({error: "username is null, try again with a valid string"})}
+  if (newUsername === null | newUsername === "") { return res.json({ error: "username is null, try again with a valid string" }) }
 
   try {
     let findUser = await User.findOne({
       username: newUsername
     })
 
-    if(findUser){ //if username was created before, the program will return the data asociated with that user.
+    if (findUser) { //if username was created before, the program will return the data asociated with that user.
       res.json({
         username: findUser.username,
         _id: findUser._id
@@ -65,13 +65,154 @@ app.post('/api/users', async function(req, res){
       })
     }
   }
-  catch(error) {
+  catch (error) {
     console.error(error);
-    res.status(500).json({error: 'An error has ocurred'})
+    res.status(500).json({ error: 'An error has ocurred while posting a username' })
   }
 
 
 })
 
+//GET all the users in the database
+app.get('/api/users', async function (req, res) {
+
+  try {
+    let usersArray = await User.find({}, 'username _id');
+    res.json(usersArray);
+  }
+  catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error has ocurred while getting al usernames in the database' })
+  }
+})
 
 
+//create the Schema to post new exercises
+const exerciseSchema = new mongoose.Schema({
+  username: String,
+  description: String,
+  duration: Number,
+  date: Date
+})
+
+//Create the mongoose model asociated with the exersice Schema and the exercises collection in the database
+const Exercise = mongoose.model('Exercise', exerciseSchema, 'exercises');
+
+//POST a new exercise for an specific _id with the parameters: description, duration and date(optional)
+app.post('/api/users/:_id/exercises', async function (req, res) {
+  try {
+    const validUser = await User.findOne({ _id: req.params._id });
+    //validate the ID requested before continue.
+    if (!validUser) {
+      return res.json({ error: 'ID not found, try again with a valid ID.' })
+    } else {
+      try {
+
+        //validate description and duration parameters.
+        if (!req.body.description | !req.body.duration) {
+          return res.json({ error: 'invalid description or duration data. Try again with a valid string for -description- and a valid number for -duration-. If no -date- is supplied, the current date will be used.' })
+        }
+        //select between the date supplied by de user or the current date if no date was supplied.
+        let dateSupplied = null;
+        if (req.body.date === null | req.body.date === "") {
+          let currentDate = new Date();
+          dateSupplied = new Date(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`)
+        } else {
+          dateSupplied = new Date(req.body.date);
+        }
+
+
+        let findExercise = await Exercise.findOne({
+          username: validUser.username,
+          description: req.body.description,
+          duration: req.body.duration,
+          date: dateSupplied
+        })
+
+        if (findExercise) {
+          return res.json({
+            username: findExercise.username,
+            description: findExercise.description,
+            duration: findExercise.duration,
+            date: findExercise.date.toDateString(),
+            note: 'The exercise has already been added to the database'
+          })
+        } else {
+          let newExercise = new Exercise({
+            username: validUser.username,
+            description: req.body.description,
+            duration: req.body.duration,
+            date: dateSupplied
+          })
+
+          newExercise.save();
+          res.json({
+            username: newExercise.username,
+            description: newExercise.description,
+            duration: newExercise.duration,
+            date: newExercise.date.toDateString(),
+            _id: newExercise._id
+          })
+        }
+      }
+
+      catch (error2) {
+        console.error(error2);
+        res.status(500).json({ error: 'An error has ocurred while posting an exercise' })
+      }
+
+    }
+  }
+  catch (error1) {
+    console.error(error1);
+    res.status(500).json({ error: 'ID not found, try again with a valid ID.' })
+  }
+
+})
+
+//GET the logs of exercise that belong to a specific user in the format:
+// {
+//   username: "fcc_test",
+//   count: 1,
+//   _id: "5fb5853f734231456ccb3b05",
+//   log: [{
+//     description: "test",
+//     duration: 60,
+//     date: "Mon Jan 01 1990",
+//   }]
+// }
+app.get('/api/users/:_id/logs', async function (req, res) {
+  //get the username
+  try {
+
+    let findUser = await User.findOne({ _id: req.params._id });
+    if (!findUser) {
+      return res.json({ error: 'User not found. Try again with a valid ID' });
+    }
+    try {
+      let exercisesArray = await Exercise.find({ username: findUser.username }, 'description duration date');
+      let exercisesArrayParsed = [];
+      for (let i = 0; i < exercisesArray.length; i++) {
+        exercisesArrayParsed.push({
+          description: exercisesArray[i].description,
+          duration: exercisesArray[i].duration,
+          date: exercisesArray[i].date.toDateString()
+        })
+      }
+      res.json({
+        username: findUser.username,
+        count: exercisesArray.length,
+        _id: findUser._id,
+        log: exercisesArrayParsed
+      })
+    }
+    catch (error2) {
+      console.log(error2);
+      res.status(500).json({ error: 'An error has ocurred while getting the logs, please try again' })
+    }
+  }
+  catch (error1) {
+    console.error(error1);
+    res.status(500).json({ error: 'An error has ocurred while getting the user, please try again' });
+  }
+})
